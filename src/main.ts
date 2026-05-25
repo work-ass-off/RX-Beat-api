1ef1d24a5eea72ee5a59d13f4a9e189ec2f02a92
Anna Vasilevich <dzichonka@gmail.com>
2026-05-25 20:12:36 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,18 @@ async function bootstrap() {
   // class-transformer @Exclude(), @Expose(), @Transform()....
   // outgoing response
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('RX-Beat API')
+    .setDescription(
+      'API for RX-Beat application - a platform for saving and sharing music playlists',
+    )
+    .setVersion('1.0')
+    .addTag('music')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
